2d6a268196391d1afb43120d8189e8c012e61b7a
Riku Kuikka <riku.kuikka@relexsolutions.com>
2020-05-04 19:12:46 +0300

    Removed extra stuff

--- public/product-test.js
+++
@@ -1,9 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-.then(res => {
-  if(res.ok) {
-    return res;
-  } else {
-    throw Error(`Request rejected with status ${res.status}`);
-  }
-})
-.catch(console.error)
