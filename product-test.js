3014da120bf5529089718c74525c684d261217c8
Riku Kuikka <riku.kuikka@relexsolutions.com>
2020-05-04 17:10:25 +0300

    More testing

--- product-test.js
+++ product-test.js
@@ -1,3 +1,28 @@
-fetch('https://my-json-server.typicode.com/rkuikka/connecttest/products/'+Math.floor((Math.random() * 3)+1))
-  .then(response => response.json())
-  .then(json => console.log(json))
+  var getJSON = function(url, callback) {
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.responseType = 'json';
+
+      xhr.onload = function() {
+
+          var status = xhr.status;
+
+          if (status == 200) {
+              callback(null, xhr.response);
+          } else {
+              callback(status);
+          }
+      };
+
+      xhr.send();
+  };
+
+  getJSON('https://my-json-server.typicode.com/rkuikka/connecttest/products/'+Math.floor((Math.random() * 3)+1),  function(err, data) {
+
+      if (err != null) {
+          console.error(err);
+      } else {
+
+      }
+  });
